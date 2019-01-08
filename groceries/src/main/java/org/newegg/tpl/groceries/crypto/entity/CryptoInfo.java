edefdeebb9253bac06fe01ba621b3bed4c4ae2dc
purem <40200723+purem365@users.noreply.github.com>
2019-01-08 23:40:00 +0800

    Add files via upload

---
+++ groceries/src/main/java/org/newegg/tpl/groceries/crypto/entity/CryptoInfo.java
@@ -0,0 +1,31 @@
+package org.newegg.tpl.groceries.crypto.entity;
+
+public class CryptoInfo {
+    private String message;
+    private int type;
+    private String method;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
