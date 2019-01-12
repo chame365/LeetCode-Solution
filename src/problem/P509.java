8b39984225fc3319b3b3caad7e919004fc584112
purem365 <purem365@gmail.com>
2019-01-12 09:38:22 +0800

    add P509

---
+++ src/problem/P509.java
@@ -0,0 +1,17 @@
+package problem;
+
+public class P509 {
+    public static void main(String[] args) {
+        new P509().fib(5);
+    }
+
+    public int fib(int N) {
+        if(N == 0){
+            return 0;
+        }else if (N == 1){
+            return 1;
+        }else{
+            return fib(N-1) + fib(N-2);
+        }
+    }
+}
